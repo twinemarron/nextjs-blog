65801ce6f0bf263ad46845ca5d9c4187e3e7df34
twinemarron <twinemarron@gmail.com>
2021-05-25 23:53:13 +0900

    Modify little bit...

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,7 +19,7 @@ export default function Home({ allPostsData }: {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>hello world!!!!</p>
+        <p>hello world...</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
